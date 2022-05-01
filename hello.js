2edebc11d79e729d599836de0e95467a10f3429b
lSkadooshl <104402816+lSkadooshl@users.noreply.github.com>
2022-04-30 19:51:55 -0500

    did some changes

--- hello.js
+++ hello.js
@@ -3,3 +3,29 @@ console.log("i dont know war eagle");
 console.log("weegle weegle");
 
 
+let blah = -1; // integer 
+let dude = 2.5; // float 
+let hey = false; // boolean
+let hi = "howdy"; // string 
+
+console.log(blah);
+
+console.log(hi);
+
+console.log(dude);
+
+console.log(hey);
+
+if(blah < 1) { 
+    console.log("we are learning to program!");
+}
+
+
+let grassLength = 3.3;
+
+if(grassLength > 3) {
+    console.log("we are cutting grass!!!"); 
+    console.log("until i run out of batteries");
+} else {
+    console.log("going to stay on charger");
+}
